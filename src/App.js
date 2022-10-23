355f197a5dc5d4ffeb56c6a5d88f38f38cec0b51
Khanh Pham <kpp004@bucknell.edu>
2022-10-23 15:11:17 -0400

    Add testing firebase code into react

--- src/App.js
+++ src/App.js
@@ -97,8 +97,25 @@ import {getAllBluetoothInfo} from "./bluetooth.js";
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 
+//import firebase from 'firebase/app';
+import 'firebase/firestore';
+//import {useCollectionData} from 'react-firebase-hooks/firestore';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, doc, setDoc, addDoc, getDoc, getDocs } from 'firebase/firestore';
+// import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCRSc9yPEA3i9unBDcVzctM00L9gB2pCmk",
+  authDomain: "bucknell-social-ventilation.firebaseapp.com",
+  projectId: "bucknell-social-ventilation",
+  storageBucket: "bucknell-social-ventilation.appspot.com",
+  messagingSenderId: "206138886904",
+  appId: "1:206138886904:web:521c2645b5a5503537890d",
+  measurementId: "G-WHE5G8MJBS"
+};
 
 function App() {
+
   const [viewport, setViewport] = useState({
     // width: "100vw",
     // height: "100vh",
@@ -107,7 +124,6 @@ function App() {
     // zoom: 14
   });
   const [popupInfo, setPopupInfo] = useState(null);
-
   
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((pos) => {
@@ -147,6 +163,89 @@ function App() {
       )),
     []
   );
+useEffect(async()=>{
+  
+const firebaseApp = initializeApp(firebaseConfig);
+// const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
+
+//Delete collection in command line: 
+//firebase login
+//firebase firestore:delete -r collectionName --project bucknell-social-ventilation
+
+// Upload data manually
+const locationRef = collection(db, "Dana116");
+
+await setDoc(doc(locationRef, "CO2_1"), {
+    category: "University",
+    co2: [1534183990,10,300,400], //actually just all number, so can be [1534183990, 10, 200, 300] as [timestamp, timestep, all CO2 level]
+    image: "https://i.etsystatic.com/5514600/r/il/bad90f/714204774/il_570xN.714204774_3g8y.jpg"
+});
+
+await setDoc(doc(locationRef, "CO2_2"), {
+    category: "University",
+    co2: [153419000,10,200,400], 
+    image: "https://i.etsystatic.com/5514600/r/il/bad90f/714204774/il_570xN.714204774_3g8y.jpg"
+});
+
+const locationRef1 = collection(db, "Cole10");
+await setDoc(doc(locationRef1, "CO2_1"), {
+    category: "University",
+    co2: [1534183990,10,300,400],
+    image: "https://i.etsystatic.com/5514600/r/il/bad90f/714204774/il_570xN.714204774_3g8y.jpg"
+});
+
+await setDoc(doc(locationRef1, "CO2_2"), {
+    category: "University",
+    co2: [2534183990,10,400,200],
+    image: "https://i.etsystatic.com/5514600/r/il/bad90f/714204774/il_570xN.714204774_3g8y.jpg"
+});
+
+
+// Getting data manually
+const docRef1 = doc(db, "Dana116", "CO2_1");
+const docSnap1 = await getDoc(docRef1);
+
+if (docSnap1.exists()) {
+    console.log("Document data:", docSnap1.data());
+} else {
+    // doc.data() will be undefined in this case
+    console.log("No such document!");
+}
+
+const docRef2 = doc(db, "Dana116", "CO2_2");
+const docSnap2 = await getDoc(docRef2);
+
+if (docSnap2.exists()) {
+    console.log("Document data:", docSnap2.data());
+} else {
+    // doc.data() will be undefined in this case
+    console.log("No such document!");
+}
+
+const docRef3 = doc(db, "Cole10", "CO2_1");
+const docSnap3 = await getDoc(docRef3);
+
+if (docSnap3.exists()) {
+    console.log("Document data:", docSnap3.data());
+} else {
+    // doc.data() will be undefined in this case
+    console.log("No such document!");
+}
+
+const docRef4 = doc(db, "Cole10", "CO2_2");
+const docSnap4 = await getDoc(docRef4);
+
+if (docSnap4.exists()) {
+    console.log("Document data:", docSnap4.data());
+} else {
+    // doc.data() will be undefined in this case
+    console.log("No such document!");
+}
+
+},[]);
+
+
   
   return (
     <div className='map-container'>
@@ -180,3 +279,5 @@ function App() {
 };
 
 export default App;
+
+
--- src/index.js
+++ src/index.js
@@ -1,89 +1,15 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js';
-import { getFirestore, collection, doc, setDoc, addDoc, getDoc, getDocs } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js';
-// import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js';
-const firebaseConfig = {
-    apiKey: "AIzaSyCRSc9yPEA3i9unBDcVzctM00L9gB2pCmk",
-    authDomain: "bucknell-social-ventilation.firebaseapp.com",
-    projectId: "bucknell-social-ventilation",
-    storageBucket: "bucknell-social-ventilation.appspot.com",
-    messagingSenderId: "206138886904",
-    appId: "1:206138886904:web:521c2645b5a5503537890d",
-    measurementId: "G-WHE5G8MJBS"
-};
-const firebaseApp = initializeApp(firebaseConfig);
-// const auth = getAuth(firebaseApp);
-const db = getFirestore(firebaseApp);
-
-//Delete collection in command line: 
-//firebase login
-//firebase firestore:delete -r collectionName --project bucknell-social-ventilation
-
-// Upload data manually
-const locationRef = collection(db, "Dana116");
-
-await setDoc(doc(locationRef, "CO2_1"), {
-    category: "University",
-    co2: [1534183990,10,300,400], //actually just all number, so can be [1534183990, 10, 200, 300] as [timestamp, timestep, all CO2 level]
-    image: "https://i.etsystatic.com/5514600/r/il/bad90f/714204774/il_570xN.714204774_3g8y.jpg"
-});
-
-await setDoc(doc(locationRef, "CO2_2"), {
-    category: "University",
-    co2: [153419000,10,200,400], 
-    image: "https://i.etsystatic.com/5514600/r/il/bad90f/714204774/il_570xN.714204774_3g8y.jpg"
-});
-
-const locationRef1 = collection(db, "Cole10");
-await setDoc(doc(locationRef1, "CO2_1"), {
-    category: "University",
-    co2: [1534183990,10,300,400],
-    image: "https://i.etsystatic.com/5514600/r/il/bad90f/714204774/il_570xN.714204774_3g8y.jpg"
-});
-
-await setDoc(doc(locationRef1, "CO2_2"), {
-    category: "University",
-    co2: [2534183990,10,400,200],
-    image: "https://i.etsystatic.com/5514600/r/il/bad90f/714204774/il_570xN.714204774_3g8y.jpg"
-});
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import './index.css';
+import App from './App';
+ 
+ReactDOM.render(
+  <React.StrictMode>
+    
+    <App />
+  </React.StrictMode>,
+document.getElementById('root')
+);
 
 
-// Getting data manually
-const docRef1 = doc(db, "Dana116", "CO2_1");
-const docSnap1 = await getDoc(docRef1);
-
-if (docSnap1.exists()) {
-    console.log("Document data:", docSnap1.data());
-} else {
-    // doc.data() will be undefined in this case
-    console.log("No such document!");
-}
-
-const docRef2 = doc(db, "Dana116", "CO2_2");
-const docSnap2 = await getDoc(docRef2);
-
-if (docSnap2.exists()) {
-    console.log("Document data:", docSnap2.data());
-} else {
-    // doc.data() will be undefined in this case
-    console.log("No such document!");
-}
-
-const docRef3 = doc(db, "Cole10", "CO2_1");
-const docSnap3 = await getDoc(docRef3);
-
-if (docSnap3.exists()) {
-    console.log("Document data:", docSnap3.data());
-} else {
-    // doc.data() will be undefined in this case
-    console.log("No such document!");
-}
-
-const docRef4 = doc(db, "Cole10", "CO2_2");
-const docSnap4 = await getDoc(docRef4);
-
-if (docSnap4.exists()) {
-    console.log("Document data:", docSnap4.data());
-} else {
-    // doc.data() will be undefined in this case
-    console.log("No such document!");
-}
